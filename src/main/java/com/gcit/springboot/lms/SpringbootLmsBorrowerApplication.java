f3036e8d6b12f3d040a79a8e2fc65355ddc85069
unknown <deepikaravi1312@gmail.com>
2018-03-25 16:08:19 -0400

    first comit

---
+++ src/main/java/com/gcit/springboot/lms/SpringbootLmsBorrowerApplication.java
@@ -0,0 +1,29 @@
+/*package com.gcit.springboot.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLmsBorrowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLmsBorrowerApplication.class, args);
+	}
+}
+*/
+
+package com.gcit.springboot.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLmsBorrowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLmsBorrowerApplication.class, args);
+		
+		System.out.println("Hello Borrower!");
+
+	}
+}
